6fe57d24473000029304e080d12cc7c05787ad87
Andrew Cruickshank <cruickshankatc@gmail.com>
2023-07-17 16:32:48 -0400

    Cleaned up and fixed code with the help of Chat GPT. Currently, sort() is broken. Plan to fix it in next commit.

--- main.js
+++ main.js
@@ -1,302 +1,95 @@
-let presidents = [
-  {
-    name: "George Washington",
-    number: 1,
-    party: "Independent",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2865,
-    image: function() {
-      let fullName = "";
-      let fullName2;
-      for (x = 0; x < this.name.split(" ").length; x++) {
-        fullName += this.name.split(" ")[x];
-      }
+class President {
+  constructor(name, number, party, terms, diedInOffice, daysInOffice) {
+    this.name = name;
+    this.number = number;
+    this.party = party;
+    this.terms = terms;
+    this.diedInOffice = diedInOffice;
+    this.daysInOffice = daysInOffice;
+  }
 
-      fullName2 = fullName.replaceAll('.','')
-      return fullName2.toLowerCase() + ".jpg";
-    },
-  },
-  {
-    name: "John Adams",
-    number: 2,
-    party: "Federalist",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1460,
-  },
-  {
-    name: "Thomas Jefferson",
-    number: 3,
-    party: "Democratic-Republican",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2922,
-  },
-  {
-    name: "James Madison",
-    number: 4,
-    party: "Democratic-Republican",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2922,
-  },
-  {
-    name: "James Monroe",
-    number: 5,
-    party: "Democratic-Republican",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2922,
-  },
-  {
-    name: "John Quincy Adams",
-    number: 6,
-    party: "Democratic-Republican",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1461,
-  },
-  {
-    name: "Andrew Jackson",
-    number: 7,
-    party: "Democrat",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2922,
-  },
-  {
-    name: "Martin Van Buren",
-    number: 8,
-    party: "Democrat",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1461,
-  },
-  {
-    name: "William Henry Harrison",
-    number: 9,
-    party: "Whig",
-    terms: 1,
-    diedInOffice: true,
-    daysInOffice: 31,
-  },
-  {
-    name: "John Tyler",
-    number: 10,
-    party: "Independent",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1430,
-  },
-  {
-    name: "James K. Polk",
-    number: 11,
-    party: "Democrat",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1461,
-  },
-  {
-    name: "Zachary Taylor",
-    number: 12,
-    party: "Whig",
-    terms: 1,
-    diedInOffice: true,
-    daysInOffice: 492,
-  },
-  {
-    name: "Millard Fillmore",
-    number: 13,
-    party: "Whig",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 969,
-  },
-  {
-    name: "Franklin Pierce",
-    number: 14,
-    party: "Democrat",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1461,
-  },
-  {
-    name: "James Buchanan",
-    number: 15,
-    party: "Democrat",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1461,
-  },
-  {
-    name: "Abraham Lincoln",
-    number: 16,
-    party: "Republican",
-    terms: 2,
-    diedInOffice: true,
-    daysInOffice: 1503,
-  },
-  {
-    name: "Andrew Johnson",
-    number: 17,
-    party: "Democrat",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1419,
-  },
-  {
-    name: "Ulysses S. Grant",
-    number: 18,
-    party: "Republican",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2922,
-  },
-  {
-    name: "Rutherford B. Hayes",
-    number: 19,
-    party: "Republican",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1461,
-  },
-  {
-    name: "James A. Garfield",
-    number: 20,
-    party: "Republican",
-    terms: 1,
-    diedInOffice: true,
-    daysInOffice: 199,
-  },
-  {
-    name: "Chester A. Arthur",
-    number: 21,
-    party: "Republican",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1262,
-  },
-  {
-    name: "Grover Cleveland",
-    number: 22,
-    party: "Democrat",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2922,
-  },
-  {
-    name: "Benjamin Harrison",
-    number: 23,
-    party: "Republican",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1461
-  },
-  {
-    name: "William McKinley",
-    number: 25,
-    party: "Republican",
-    terms: 2,
-    diedInOffice: true,
-    daysInOffice: 1654,
-  },
-  {
-    name: "Theodore Roosevelt",
-    number: 26,
-    party: "Republican",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2728,
-  },
-  {
-    name: "William Howard Taft",
-    number: 27,
-    party: "Republican",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1461
-  },
-  {
-    name: "Woodrow Wilson",
-    number: 28,
-    party: "Democrat",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2922,
-  },
-  {
-    name: "Warren G. Harding",
-    number: 29,
-    party: "Republican",
-    terms: 1,
-    diedInOffice: true,
-    daysInOffice: 881,
-  },
-  {
-    name: "Calvin Coolidge",
-    number: 30,
-    party: "Republican",
-    terms: 2,
-    diedInOffice: false,
-    daysInOffice: 2041,
-  },
-  {
-    name: "Herbert Hoover",
-    number: 31,
-    party: "Republican",
-    terms: 1,
-    diedInOffice: false,
-    daysInOffice: 1461,
-  },
-  {
-    name: "Franklin D. Roosevelt",
-    number: 32,
-    party: "Democrat",
-    terms: 4,
-    diedInOffice: true,
-    daysInOffice: 4422,
-  },
-]
+  image() {
+    let fullName = "";
+    let fullName2;
+    for (x = 0; x < this.name.split(" ").length; x++) {
+      fullName += this.name.split(" ")[x];
+    }
+
+    fullName2 = fullName.replaceAll('.','')
+    return fullName2.toLowerCase() + ".jpg";
+  }
+}
+
+let presidents = [
+  new President("George Washington", 1, "Independent", 2, false, 2865),
+  new President("John Adams", 2, "Federalist", 1, false, 1460),
+  new President("Thomas Jefferson", 3, "Democratic-Republican", 2, false, 2922),
+  new President("James Madison", 4, "Democratic-Republican", 2, false, 2922),
+  new President("James Monroe", 5, "Democratic-Republican", 2, false, 2922),
+  new President("John Quincy Adams", 6, "Democratic-Republican", 1, false, 1461),
+  new President("Andrew Jackson", 7, "Democrat", 2, false, 2922),
+  new President("Martin Van Buren", 8, "Democrat", 1, false, 1461),
+  new President("William Henry Harrison", 9, "Whig", 1, true, 31),
+  new President("John Tyler", 10, "Independent", 1, false, 1430),
+  new President("James K. Polk", 11, "Democrat", 1, false, 1461),
+  new President("Zachary Taylor", 12, "Whig", 1, true, 492),
+  new President("Millard Fillmore", 13, "Whig", 1, false, 969),
+  new President("Franklin Pierce", 14, "Democrat", 1, false, 1461),
+  new President("James Buchanan", 15, "Democrat", 1, false, 1461),
+  new President("Abraham Lincoln", 16, "Republican", 2, true, 1503),
+  new President("Andrew Johnson", 17, "Democrat", 1, false, 1419),
+  new President("Ulysses S. Grant", 18, "Republican", 2, false, 2922),
+  new President("Rutherford B. Hayes", 19, "Republican", 1, false, 1461),
+  new President("James A. Garfield", 20, "Republican", 1, true, 199),
+  new President("Chester A. Arthur", 21, "Republican", 1, false, 1262),
+  new President("Grover Cleveland", 22, "Democrat", 2, false, 2922),
+  new President("Benjamin Harrison", 23, "Republican", 1, false, 1461),
+  new President("William McKinley", 25, "Republican", 2, true, 1654),
+  new President("Theodore Roosevelt", 26, "Republican", 2, false, 2728),
+  new President("William Howard Taft", 27, "Republican", 1, false, 1461),
+  new President("Woodrow Wilson", 28, "Democrat", 2, false, 2922),
+  new President("Warren G. Harding", 29, "Republican", 1, true, 881),
+  new President("Calvin Coolidge", 30, "Republican", 2, false, 2041),
+  new President("Herbert Hoover", 31, "Republican", 1, false, 1461),
+  new President("Franklin D. Roosevelt", 32, "Democrat", 4, true, 4422),
+];
 
 //VARIABLES AND INITIAL SETTINGS
 let main = document.getElementById("main");
 
-//The "names" variable contains an array of whatever presidents are currently on display (whether the initial list or a filtered form of the list). The elements in the array are all still in object form. They don't only contain the President's name but also other properties (party, terms, diedInOffice, etc.) This value must remain in the global scope as it needs to be accessed by sort and other functions to be added.
-let names;
-
-makeList();
+// Call the displayPresidents function initially to populate the list
+displayPresidents(presidents);
 
 //FUNCTIONS
-function makeList() {
-  presidents = presidents.sort((a, b) => {
-    return a.number - b.number;
-  });
-  
-  names = presidents;
-  
-  x = presidents.map(president => {
-    return `<li>${president.name}</li>`
-  });
-  
-  names2 = x.join().replaceAll(",", "");
-  main.innerHTML = names2;
+// Function to display all presidents' names in order
+function displayPresidents(presidentsArray) {
+  const sortedPresidents = presidentsArray.slice().sort((a, b) => a.number - b.number);
+
+  // Create an array of list items using template literals
+  const listItems = sortedPresidents.map(president => `<li>${president.name}</li>`);
+
+  // Set the HTML content of the 'main' element
+  main.innerHTML = listItems.join('');
+}
+
+// Function to reset the list to its initial state
+function resetList() {
+  displayPresidents(presidents);
 }
 
 
 function byParty() {
-  let selector = document.getElementById("partySelector");
-  
-  names = presidents.filter((president) => {
-    return president.party === selector.value;
-  });
+  const partySelector = document.getElementById('partySelector');
+  const selectedParty = partySelector.value;
 
-  names2 = names.map(name => {
-    return `<li>${name.name}</li>`
-  })
+  if (selectedParty === '') {
+    // If no party is selected, reset the list to the initial state
+    resetList();
+    return;
+  }
 
-  names2 = names2.join().replaceAll(",", ""); 
-  main.innerHTML = names2;  
+  const filteredPresidents = presidents.filter(president => president.party === selectedParty);
+  displayPresidents(filteredPresidents);
 }
 
 function sort() {
@@ -311,3 +104,5 @@ function sort() {
   names2 = names2.join().replaceAll(",", ""); 
   main.innerHTML = names2;  
 }
+
+//document.getElementById("little").innerHTML = `<img src="images/${presidents[0].image()}">`;
